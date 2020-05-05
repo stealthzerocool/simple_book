6eac9335b92cb6ec1421bc228f1cfbbce48123be
stealthzerocool <ananthuk11@gmail.com>
2020-05-06 01:41:27 +0530

    changes to admin

--- admin/admin_profile.php
+++ admin/admin_profile.php
@@ -7,12 +7,13 @@ while($res=mysqli_fetch_assoc($sql))
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hotel.Com</title>
+	<title>Books</title>
 	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
 </head>
 <body>
 <h1 style="background-color:#fff;border-radius:50px;text-align:center;font-family: 'Libre Baskerville', serif;box-shadow:5px 5px 9px white;text-shadow:2px 2px #fff;">Admin Profile</h1><br>
-<center></center>
+<center>
+<img src="../image/aa.png" style="width:200px;height:200px;background-color:white;"></center>
 <div class="container"style="width:100%;">
   <form action="/action_page.php">
     <div class="form-group">
